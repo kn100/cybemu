9eb7e3eb21aeba463fd77f2acd003abe4c761799
Kevin <kn100@kn100.me>
2022-04-21 00:51:33 +0100

    refactor Table232 entirely

--- main.go
+++ main.go
@@ -31,9 +31,9 @@ func main() {
 	//
 	instructions := []Inst{}
 	// Read entire file into byte array
-	// TODO: Set array to correct size using os.Stat
 	bytes := make([]byte, finfo.Size())
 	f.Read(bytes)
+
 	i := 0
 	for i < len(bytes) {
 		// If we have less than 2 bytes left, we are done
@@ -58,14 +58,14 @@ func main() {
 				if BH&0x8 == 0 {
 					inst.Opcode = "stc"
 				} else {
-					inst.Opcode = "???word???"
+					inst.Opcode = ".word"
 				}
 			case 0x3:
 				BH := bytes[i+1] >> 4
 				if BH&0x8 == 0 {
 					inst.Opcode = "ldc"
 				} else {
-					inst.Opcode = "???word???"
+					inst.Opcode = ".word"
 				}
 			case 0x4:
 				inst.Opcode = "orc"
@@ -236,300 +236,237 @@ func Table232(bytes []byte) (string, int) {
 	AH := bytes[0] >> 4
 	AL := bytes[0] & 0x0F
 	BH := bytes[1] >> 4
-	switch {
-	case AH == 0x0 && AL == 0x1:
-		switch {
-		case BH == 0x0:
-			return "mov", size
-		case BH == 0x1 || BH == 0x2 || BH == 0x3:
-			if BH&0x8 == 0 {
-				return "ldm", size
-			} else {
-				return "stm", size
+	switch AH {
+	case 0x0:
+		switch AL {
+		case 0x1:
+			switch BH {
+			case 0x0:
+				return "mov", size
+			case 0x1, 0x2, 0x3:
+				if BH&0x8 == 0 {
+					return "ldm", size
+				} else {
+					return "stm", size
+				}
+			case 0x4:
+				if BH&0x8 == 0 {
+					return "ldc", size
+				} else {
+					return "stc", size
+				}
+			case 0x8:
+				return "sleep", size
+			case 0xC:
+				return Table233(bytes)
+			case 0xD:
+				return Table233(bytes)
+			case 0xE:
+				return "tas", size
+			case 0xF:
+				return Table233(bytes)
+			default:
+				return ".word", size
 			}
-		case BH == 0x4:
-			if BH&0x8 == 0 {
-				return "ldc", size
-			} else {
-				return "stc", size
+		case 0xA:
+			switch BH {
+			case 0x0:
+				return "inc", size
+			case 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF:
+				return "add", size
+			default:
+				return ".word", size
 			}
-		// 0x5, 0x6, 0x7, are illegal
-		case BH == 0x8:
-			return "sleep", size
-		// 0x9, 0xA, and 0xB are illegal
-		case BH == 0xC:
-			return Table233(bytes)
-		case BH == 0xD:
-			return Table233(bytes)
-		case BH == 0xE:
-			return "tas", size
-		case BH == 0xF:
-			return Table233(bytes)
-		default:
-			return "???word???", size
+		case 0xB:
+			switch BH {
+			case 0x0:
+				return "adds", size
+			case 0x5:
+				return "inc", size
+			case 0x7:
+				return "inc", size
+			case 0x8, 0x9:
+				return "adds", size
+			case 0xD:
+				return "inc", size
+			case 0xF:
+				return "inc", size
+			default:
+				return ".word", size
+			}
+		case 0xF:
+			switch BH {
+			case 0x0:
+				return "daa", size
+			case 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF:
+				return "mov", size
+			default:
+				return ".word", size
+			}
 		}
-	case AH == 0x0 && AL == 0xA:
-		switch {
-		case BH == 0x0:
-			return "inc", size
-		//0x1 to 0x7 are illegal
-		case BH == 0x8 || BH == 0x9 || BH == 0xA || BH == 0xB || BH == 0xC || BH == 0xD || BH == 0xE || BH == 0xF:
-			return "add", size
-		default:
-			return "???word???", size
+	case 0x1:
+		switch AL {
+		case 0x0:
+			switch BH {
+			case 0x0, 0x1:
+				return "shll", size
+			case 0x3, 0x4, 0x5:
+				return "shll", size
+			case 0x7:
+				return "shll", size
+			case 0x8, 0x9:
+				return "shal", size
+			case 0xB, 0xC, 0xD:
+				return "shal", size
+			case 0xF:
+				return "shal", size
+			default:
+				return ".word", size
+			}
+		case 0x1:
+			switch BH {
+			case 0x0, 0x1:
+				return "shlr", size
+			case 0x3, 0x4, 0x5:
+				return "shlr", size
+			case 0x7:
+				return "shlr", size
+			case 0x8, 0x9:
+				return "shar", size
+			case 0xB, 0xC, 0xD:
+				return "shar", size
+			case 0xF:
+				return "shar", size
+			default:
+				return ".word", size
+			}
+		case 0x2:
+			switch BH {
+			case 0x0, 0x1, 0x3, 0x4, 0x5, 0x7:
+				return "rotxl", size
+			case 0x8, 0x9, 0xB, 0xC, 0xD, 0xF:
+				return "rotl", size
+			default:
+				return ".word", size
+			}
+		case 0x3:
+			switch BH {
+			case 0x0, 0x1, 0x3, 0x4, 0x5, 0x7:
+				return "rotxr", size
+			case 0x8, 0x9, 0xB, 0xC, 0xD, 0xF:
+				return "rotr", size
+			default:
+				return ".word", size
+			}
+		case 0x7:
+			switch BH {
+			case 0x0, 0x1, 0x3:
+				return "not", size
+			case 0x5, 0x7:
+				return "extu", size
+			case 0x8, 0x9, 0xB:
+				return "neg", size
+			case 0xD, 0xF:
+				return "exts", size
+			default:
+				return ".word", size
+			}
+		case 0xA:
+			switch BH {
+			case 0x0:
+				return "dec", size
+			case 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF:
+				return "sub", size
+			default:
+				return ".word", size
+			}
+		case 0xB:
+			switch BH {
+			case 0x0:
+				return "subs", size
+			case 0x5, 0x7, 0xD, 0xF:
+				return "dec", size
+			case 0x8, 0x9:
+				return "subs", size
+			default:
+				return ".word", size
+			}
+		case 0xF:
+			switch BH {
+			case 0x0:
+				return "das", size
+			case 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF:
+				return "cmp", size
+			default:
+				return ".word", size
+			}
 		}
-	case AH == 0x0 && AL == 0xB:
-		switch {
-		case BH == 0x0:
-			return "adds", size
-		// 0x1 to 0x4 are illegal
-		case BH == 0x5:
-			return "inc", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "inc", size
-		case BH == 0x8 || BH == 0x9:
-			return "adds", size
-		// 0xA, to 0xC are illegal
-		case BH == 0xD:
-			return "inc", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "inc", size
-		default:
-			return "???word???", size
+	case 0x5:
+		switch AL {
+		case 0x8:
+			return Branches(BH), size
 		}
-	case AH == 0x0 && AL == 0xF:
-		switch {
-		case BH == 0x0:
-			return "daa", size
-		// 0x1 to 0x7 are illegal
-		case BH == 0x8 || BH == 0x9 || BH == 0xA || BH == 0xB || BH == 0xC || BH == 0xD || BH == 0xE || BH == 0xF:
-			return "mov", size
-		default:
-			return "???word???", size
+	case 0x6:
+		switch AL {
+		case 0xA:
+			switch BH {
+			case 0x0:
+				return "mov", size
+			case 0x1, 0x3:
+				return Table234(bytes)
+			case 0x2, 0xA, 0x8:
+				return "mov", size
+			case 0x4:
+				return "movfpe", size
+			case 0xC:
+				return "movtpe", size
+			default:
+				return ".word", size
+			}
 		}
-	case AH == 0x1 && AL == 0x0:
-		switch {
-		case BH == 0x0 || BH == 0x1:
-			return "shll", size
-		// 0x2 is illegal
-		case BH == 0x3 || BH == 0x4 || BH == 0x5:
-			return "shll", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "shll", size
-		case BH == 0x8 || BH == 0x9:
-			return "shal", size
-		// 0xA is illegal
-		case BH == 0xB || BH == 0xC || BH == 0xD:
-			return "shal", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "shal", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0x1:
-		switch {
-		case BH == 0x0 || BH == 0x1:
-			return "shlr", size
-		// 0x2 is illegal
-		case BH == 0x3 || BH == 0x4 || BH == 0x5:
-			return "shlr", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "shlr", size
-		case BH == 0x8 || BH == 0x9:
-			return "shar", size
-		// 0xA is illegal
-		case BH == 0xB || BH == 0xC || BH == 0xD:
-			return "shar", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "shar", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0x2:
-		switch {
-		case BH == 0x0 || BH == 0x1:
-			return "rotxl", size
-		// 0x2 is illegal
-		case BH == 0x3 || BH == 0x4 || BH == 0x5:
-			return "rotxl", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "rotxl", size
-		case BH == 0x8 || BH == 0x9:
-			return "rotl", size
-		// 0xA is illegal
-		case BH == 0xB || BH == 0xC || BH == 0xD:
-			return "rotl", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "rotl", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0x3:
-		switch {
-		case BH == 0x0 || BH == 0x1:
-			return "rotxr", size
-		// 0x2 is illegal
-		case BH == 0x3 || BH == 0x4 || BH == 0x5:
-			return "rotxr", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "rotxr", size
-		case BH == 0x8 || BH == 0x9:
-			return "rotr", size
-		// 0xA is illegal
-		case BH == 0xB || BH == 0xC || BH == 0xD:
-			return "rotr", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "rotr", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0x7:
-		switch {
-		case BH == 0x0 || BH == 0x1:
-			return "not", size
-		// 0x2 is illegal
-		case BH == 0x3:
-			return "not", size
-		// 0x4 is illegal
-		case BH == 0x5:
-			return "extu", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "extu", size
-		case BH == 0x8 || BH == 0x9:
-			return "neg", size
-		// 0xA is illegal
-		case BH == 0xB:
-			return "neg", size
-		// 0xC is illegal
-		case BH == 0xD:
-			return "exts", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "exts", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0xA:
-		switch {
-		case BH == 0x0:
-			return "dec", size
-		// 0x1 to 0x7 is illegal
-		case BH == 0x8 || BH == 0x9 || BH == 0xA || BH == 0xB || BH == 0xC || BH == 0xD || BH == 0xE || BH == 0xF:
-			return "sub", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0xB:
-		switch {
-		case BH == 0x0:
-			return "subs", size
-		// 0x1 to 0x4 is illegal
-		case BH == 0x5:
-			return "dec", size
-		// 0x6 is illegal
-		case BH == 0x7:
-			return "dec", size
-		case BH == 0x8 || BH == 0x9:
-			return "subs", size
-		// 0xA to 0xc is illegal
-		case BH == 0xD:
-			return "dec", size
-		// 0xE is illegal
-		case BH == 0xF:
-			return "dec", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x1 && AL == 0xF:
-		switch {
-		case BH == 0x0:
-			return "das", size
-		// 0x1 to 0x7 is illegal
-		case BH == 0x8 || BH == 0x9 || BH == 0xA || BH == 0xB || BH == 0xC || BH == 0xD || BH == 0xE || BH == 0xF:
-			return "cmp", size
-		default:
-			return "???word???", size
-		}
-	case AH == 0x5 && AL == 0x8:
-		return Branches(BH), size
-	case AH == 0x6 && AL == 0xA:
-		switch {
-		case BH == 0x0:
-			return "mov", size
-		case BH == 0x1:
-			return Table234(bytes)
-		case BH == 0x2:
-			return "mov", size
-		case BH == 0x3:
-			return Table234(bytes)
-		case BH == 0x4:
-			return "movfpe", size
-		// 0x5 to 0x7 is illegal
-		case BH == 0x8:
-			return "mov", size
-		// 0x9 is illegal
-		case BH == 0xA:
-			return "mov", size
-		// 0xB is illegal
-		case BH == 0xC:
-			return "movtpe", size
-		// 0xD to 0xf is illegal
-		default:
-			return "???word???", size
-		}
-	case AH == 0x7 && AL == 0x9:
-		switch {
-		case BH == 0x0:
-			return "mov", size
-		case BH == 0x1:
-			return "add", size
-		case BH == 0x2:
-			return "cmp", size
-		case BH == 0x3:
-			return "sub", size
-		case BH == 0x4:
-			return "or", size
-		case BH == 0x5:
-			return "xor", size
-		case BH == 0x6:
-			return "and", size
-		// 0x7 to 0xF is illegal
-		default:
-			return "???word???", size
-		}
-	case AH == 0x7 && AL == 0xA:
-		switch {
-		case BH == 0x0:
-			return "mov", size
-		case BH == 0x1:
-			return "add", size
-		case BH == 0x2:
-			return "cmp", size
-		case BH == 0x3:
-			return "sub", size
-		case BH == 0x4:
-			return "or", size
-		case BH == 0x5:
-			return "xor", size
-		case BH == 0x6:
-			return "and", size
-		// 0x7 to 0xF is illegal
-		default:
-			return "???word???", size
+	case 0x7:
+		switch AL {
+		case 0x9:
+			switch BH {
+			case 0x0:
+				return "mov", size
+			case 0x1:
+				return "add", size
+			case 0x2:
+				return "cmp", size
+			case 0x3:
+				return "sub", size
+			case 0x4:
+				return "or", size
+			case 0x5:
+				return "xor", size
+			case 0x6:
+				return "and", size
+			default:
+				return ".word", size
+			}
+		case 0xA:
+			switch BH {
+			case 0x0:
+				return "mov", size
+			case 0x1:
+				return "add", size
+			case 0x2:
+				return "cmp", size
+			case 0x3:
+				return "sub", size
+			case 0x4:
+				return "or", size
+			case 0x5:
+				return "xor", size
+			case 0x6:
+				return "and", size
+			default:
+				return ".word", size
+			}
 		}
 	}
-	return "???word???", size
+	return ".word", size
 }
 
 // Table2.3 (3) - returns the instruction and the size in bytes (4)
@@ -550,7 +487,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x2:
 			return "mulxs", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x0 && AL == 0x1 && BH == 0xD && BL == 0x0 && CH == 0x5:
 		switch {
@@ -559,7 +496,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x3:
 			return "divxs", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x0 && AL == 0x1 && BH == 0xF && BL == 0x0 && CH == 0x6:
 		switch {
@@ -570,7 +507,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x6:
 			return "and", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH here is actually a register field
 	case AH == 0x7 && AL == 0xC && BL == 0x0 && CH == 0x6:
@@ -578,7 +515,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x3:
 			return "btst", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH here is actually a register field
 	case AH == 0x7 && AL == 0xC && BL == 0x0 && CH == 0x7:
@@ -610,7 +547,7 @@ func Table233(bytes []byte) (string, int) {
 				return "bild", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH here is actually a register field
 	case AH == 0x7 && AL == 0xD && BL == 0x0 && CH == 0x6:
@@ -628,7 +565,7 @@ func Table233(bytes []byte) (string, int) {
 				return "bist", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH here is actually a register field
 	case AH == 0x7 && AL == 0xD && BL == 0x0 && CH == 0x7:
@@ -640,7 +577,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x2:
 			return "bclr", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH and BL here are actually an absolute address
 	case AH == 0x7 && AL == 0xE && CH == 0x6:
@@ -648,7 +585,7 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x3:
 			return "btst", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH and BL here are actually an absolute address
 	case AH == 0x7 && AL == 0xE && CH == 0x7:
@@ -680,7 +617,7 @@ func Table233(bytes []byte) (string, int) {
 				return "bild", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// BH and BL here are actually an absolute address
 	case AH == 0x7 && AL == 0xF && CH == 0x6:
@@ -698,7 +635,7 @@ func Table233(bytes []byte) (string, int) {
 				return "bist", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 		// BH and BL here are actually an absolute address
 	case AH == 0x7 && AL == 0xF && CH == 0x7:
@@ -711,10 +648,10 @@ func Table233(bytes []byte) (string, int) {
 		case CL == 0x2:
 			return "bclr", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	}
-	return "???word???", 2
+	return ".word", 2
 }
 
 func Table234(bytes []byte) (string, int) {
@@ -741,7 +678,7 @@ func Table234(bytes []byte) (string, int) {
 		case 0x3:
 			return "btst", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x1 && BL == 0x0 && EH == 0x7:
 		switch EL {
@@ -772,7 +709,7 @@ func Table234(bytes []byte) (string, int) {
 				return "bild", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x1 && BL == 0x8 && EH == 0x6:
 		switch EL {
@@ -789,7 +726,7 @@ func Table234(bytes []byte) (string, int) {
 				return "bist", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x1 && BL == 0x8 && EH == 0x7:
 		switch EL {
@@ -800,7 +737,7 @@ func Table234(bytes []byte) (string, int) {
 		case 0x2:
 			return "bclr", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	// AH 6 AL A BH 3 BL 0 GH 6
 	case AH == 0x6 && AL == 0xA && BH == 0x3 && BL == 0x0 && GH == 0x6:
@@ -808,7 +745,7 @@ func Table234(bytes []byte) (string, int) {
 		case 0x3:
 			return "btst", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x3 && BL == 0x0 && GH == 0x7:
 		switch EL {
@@ -839,7 +776,7 @@ func Table234(bytes []byte) (string, int) {
 				return "bild", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x3 && BL == 0x8 && GH == 0x6:
 		switch EL {
@@ -856,7 +793,7 @@ func Table234(bytes []byte) (string, int) {
 				return "bist", size
 			}
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	case AH == 0x6 && AL == 0xA && BH == 0x3 && BL == 0x8 && GH == 0x7:
 		switch EL {
@@ -867,10 +804,10 @@ func Table234(bytes []byte) (string, int) {
 		case 0x2:
 			return "bclr", size
 		default:
-			return "???word???", 2
+			return ".word", 2
 		}
 	default:
-		return "???word???", 2
+		return ".word", 2
 	}
 
 }
@@ -919,7 +856,7 @@ func PrintAssy(instructions []Inst) {
 	for _, inst := range instructions {
 		if inst.Opcode == "unk" {
 			unks++
-		} else if inst.Opcode == "???word???" {
+		} else if inst.Opcode == ".word" {
 			decoded++
 			word++
 		} else {
